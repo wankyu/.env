d37953b3e79dd54f7abb5b9aa80180f3fc850aa8
wankyu <wankyu@nts-corp.com>
2019-03-12 12:14:56 +0900

    add init.sh

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+envroot='~/envsetting'
+envs=(
+    .vim
+    .tmux.conf
+)
+
+grep -q LOCAL_ENV ~/.bashrc
+if [ $? -eq 0 ]
+then
+    echo 'Warning: envsetting line alrady exist in .bashrc'
+else
+    # echo 'add envsetting in .bashrc'
+    echo -e "LOCAL_ENV=${envroot};source \${LOCAL_ENV}/.bashrc\n" >> ~/.bashrc
+fi
+
+for env in "${envs[@]}"
+do
+    if [ -e ~/$env ]
+    then
+        echo Warning: "~/$env" already exist.
+    else
+        # echo $envroot/$env ~/$env
+        ln -s $envroot/$env ~/$env
+    fi
+done
+
