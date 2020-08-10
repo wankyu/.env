6cc4519fdc8dbd5859a0e9155a699b7d5ddb83c6
wankyu <wankyu@nts-corp.com>
2020-08-10 11:23:03 +0900

    update custom_PS1

--- sh/custom_PS1.sh
+++ sh/custom_PS1.sh
@@ -1,16 +1,17 @@
-PS_COLOR_White="\[\e[0;37m\]"
-PS_COLOR_BYellow="\[\e[1;33m\]"
-PS_COLOR_BWhite="\[\e[1;37m\]"
-PS_COLOR_IBlack="\[\e[0;90m\]"
-PS_COLOR_ICyan="\[\e[0;96m\]"
-PS_COLOR_OFF="\[\e[0m\]"
-PS1="[$PS_COLOR_BWhite\u$PS_COLOR_OFF@$PS_COLOR_White\h]"
-PS1+="$PS_COLOR_IBlack(\j)"
-PS1+=" $PS_COLOR_ICyan\W"
+# ANSI_escape_code
+COLOR_WHITE="\[\e[37m\]"
+COLOR_BOLDWHITE="\[\e[1;37m\]"
+COLOR_BOLDYELLOW="\[\e[1;33m\]"
+COLOR_BLACK="\[\e[90m\]"
+COLOR_CYAN="\[\e[96m\]"
+COLOR_OFF="\[\e[0m\]"
+PS1="[${COLOR_BOLDWHITE}\u${COLOR_OFF}@${COLOR_WHITE}\h]"
+PS1+="${COLOR_BLACK}(\j)"
+PS1+=" ${COLOR_CYAN}\W"
 # Load in the git branch prompt script.
 if [ -f ${LOCAL_ENV}/sh/git-prompt.sh ]; then
     source ${LOCAL_ENV}/sh/git-prompt.sh
-    PS1+="$PS_COLOR_BYellow"'$(__git_ps1)'
+    PS1+="${COLOR_BOLDYELLOW}"'$(__git_ps1)'
 fi
-PS1+="$PS_COLOR_OFF \$ "
+PS1+="${COLOR_OFF} \$ "
 
